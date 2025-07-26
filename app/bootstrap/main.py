8c0eecc58d564345ec559be4ec7208feeeda845f
wokie <contextyoo@gmail.com>
2025-07-26 22:04:34 +0300

    feature: fastapi exception handler

--- app/bootstrap/main.py
+++ app/bootstrap/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from presentation.api import v1_router
+from presentation.exceptions.handler import register_exception_handlers
 
 
 async def create_app() -> FastAPI:
@@ -12,5 +13,6 @@ async def create_app() -> FastAPI:
         docs_url="/docs",
         root_path="/api",
     )
+    register_exception_handlers(app=app)
 
     return app
