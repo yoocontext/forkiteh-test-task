c01b12bcd5ee64af22e4201e866147cdfc5ebea6
wokie <contextyoo@gmail.com>
2025-07-26 21:58:47 +0300

    feature: fastapi-entrypoint

--- app/bootstrap/main.py
+++ app/bootstrap/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from forkiteh-test-task!")
+from fastapi import FastAPI
+
+from presentation.api import v1_router
 
 
-if __name__ == "__main__":
-    main()
+async def create_app() -> FastAPI:
+    app = FastAPI(
+        debug=False,
+        routes=[v1_router],
+        title="forkiteh",
+        summary="Microservice built with FastAPI to retrieve TRON address information",
+        docs_url="/docs",
+        root_path="/api",
+    )
+
+    return app
