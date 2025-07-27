bfc01433b3635044a6ee7c60d763d71f0b8ca19a
wokie <contextyoo@gmail.com>
2025-07-27 17:53:00 +0300

    feature: create balance_trx, bandwidth, energy gateways

---
+++ app/infra/tron/gateways/get/bandwidth.py
@@ -0,0 +1,15 @@
+from tronpy import AsyncTron
+
+from infra.tron.gateways.base import BaseGateway
+from infra.tron.handlers import handle_tronpy_errors
+from share.custom_types import WalletAddress
+
+
+class GetBandwidthTronGateway(BaseGateway):
+    tron: AsyncTron
+
+    @handle_tronpy_errors
+    async def act(self, address: WalletAddress) -> int:
+        bandwidth: int = await self.tron.get_bandwidth(addr=address)
+
+        return bandwidth
