c53c30a748c15cd1d62c88d690dd01ad4bb4b4a1
wokie <contextyoo@gmail.com>
2025-07-27 15:18:12 +0300

    feature: update parameters

--- app/infra/tron/gateways/get_account/gateway.py
+++ app/infra/tron/gateways/get_account/gateway.py
@@ -5,13 +5,14 @@ from tronpy import AsyncTron
 from infra.tron.gateways.base import BaseGateway
 from infra.tron.gateways.get_account.mappers import account_response_retort
 from infra.tron.gateways.get_account.schemas import AccountResponseSchema
+from share.custom_types import WalletAddress
 
 
 @dataclass
 class GetAccountTronGateway(BaseGateway):
     tron: AsyncTron
 
-    async def act(self, address: str) -> AccountResponseSchema:
+    async def act(self, address: WalletAddress) -> AccountResponseSchema:
         account: dict = await self.tron.get_account(address)
         account_schema: AccountResponseSchema = (
             account_response_retort.load(account, AccountResponseSchema)
