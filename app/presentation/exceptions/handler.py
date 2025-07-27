0df844788be44a61c5710c1523b362c761b51d39
wokie <contextyoo@gmail.com>
2025-07-27 17:57:01 +0300

    feature: register exceptions in fastapi_exceptions_handler

--- app/presentation/exceptions/handler.py
+++ app/presentation/exceptions/handler.py
@@ -3,13 +3,21 @@ from fastapi import (
     HTTPException,
 )
 from starlette.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
     HTTP_500_INTERNAL_SERVER_ERROR,
 )
 
+from infra.tron.exceptions import (
+    BadAddressTronException,
+    AddressNotFoundTronException,
+)
 from share.exceptions import CustomException
 
 exceptions_map: dict[type[CustomException], int] = {
     CustomException: HTTP_500_INTERNAL_SERVER_ERROR,
+    BadAddressTronException: HTTP_400_BAD_REQUEST,
+    AddressNotFoundTronException: HTTP_404_NOT_FOUND,
 }
 
 def register_exception_handlers(app: FastAPI):
